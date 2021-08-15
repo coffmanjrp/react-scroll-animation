11829a52e635e266498d2c0590349ac45564633d
coffmanjrp <coffmanjrp@gmail.com>
2021-08-14 17:34:23 -0700

    Html and styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,84 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <>
+      <h1>Scroll to see the animation</h1>
+      <div className="box">
+        <h2>Content1</h2>
+      </div>
+      <div className="box">
+        <h2>Content2</h2>
+      </div>
+      <div className="box">
+        <h2>Content3</h2>
+      </div>
+      <div className="box">
+        <h2>Content4</h2>
+      </div>
+      <div className="box">
+        <h2>Content5</h2>
+      </div>
+      <div className="box">
+        <h2>Content6</h2>
+      </div>
+      <div className="box">
+        <h2>Content7</h2>
+      </div>
+      <div className="box">
+        <h2>Content8</h2>
+      </div>
+      <div className="box">
+        <h2>Content9</h2>
+      </div>
+      <div className="box">
+        <h2>Content10</h2>
+      </div>
+      <div className="box">
+        <h2>Content11</h2>
+      </div>
+      <div className="box">
+        <h2>Content12</h2>
+      </div>
+      <div className="box">
+        <h2>Content13</h2>
+      </div>
+      <div className="box">
+        <h2>Content14</h2>
+      </div>
+      <div className="box">
+        <h2>Content15</h2>
+      </div>
+      <div className="box">
+        <h2>Content16</h2>
+      </div>
+      <div className="box">
+        <h2>Content17</h2>
+      </div>
+      <div className="box">
+        <h2>Content18</h2>
+      </div>
+      <div className="box">
+        <h2>Content19</h2>
+      </div>
+      <div className="box">
+        <h2>Content20</h2>
+      </div>
+      <div className="box">
+        <h2>Content21</h2>
+      </div>
+      <div className="box">
+        <h2>Content22</h2>
+      </div>
+      <div className="box">
+        <h2>Content23</h2>
+      </div>
+      <div className="box">
+        <h2>Content24</h2>
+      </div>
+      <div className="box">
+        <h2>Content25</h2>
+      </div>
+    </>
   );
 }
 
